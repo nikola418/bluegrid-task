8ea2a9acfb10b0227d5455b5e47f3ed0a968ffd1
Nikola Milovanovic <nikolam998@gmail.com>
2024-06-17 15:23:37 +0200

    config, docs, fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  const configService = app.get(ConfigService);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder().setTitle('Bluegrid API example').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  const port = configService.getOrThrow<number>('HTTP_PORT');
+  await app.listen(port, '0.0.0.0', async () => {
+    Logger.log(
+      `🚀 Application is running! Check out the docs on: ${await app.getUrl()}/docs`,
+    );
+  });
 }
 bootstrap();
